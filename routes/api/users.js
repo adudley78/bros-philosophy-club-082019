7296a9efc59b6103e32a087d276de3dce4041d25
adudley78 <dudleyadam@gmail.com>
2019-08-22 13:23:18 -0400

    Add validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+// const config = require('config');
 
 // @route   GET api/users
-// @desc    Test route
+// @desc    Register route
 // @access  Public (no token required)
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    // See if user exists
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // Return jwt
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;

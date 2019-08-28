26bb968590596a3545a3d4e74f59692754c523c4
adudley78 <dudleyadam@gmail.com>
2019-08-27 20:48:58 -0400

    Build read and delete spark routes

--- routes/api/sparks.js
+++ routes/api/sparks.js
@@ -6,7 +6,7 @@ const Spark = require('../../models/Spark');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-// @route   GET api/sparks
+// @route   POST api/sparks
 // @desc    Create a spark
 // @access  Private
 router.post(
@@ -44,5 +44,66 @@ router.post(
     }
   }
 );
+
+// @route   GET api/sparks
+// @desc    Read all sparks
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const sparks = await Spark.find().sort({ date: -1 });
+    res.json(sparks);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/sparks/:id
+// @desc    Get spark by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const spark = await Spark.findById(req.params.id);
+
+    if (!spark) {
+      return res.status(404).json({ msg: 'Spark not found' });
+    }
+    res.json(spark);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Spark not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE api/sparks/:id
+// @desc    Delete a spark
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const spark = await Spark.findById(req.params.id);
+
+    if (!spark) {
+      return res.status(404).json({ msg: 'Spark not found' });
+    }
+
+    // Check user
+    if (spark.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await spark.remove();
+
+    res.json({ msg: 'Spark removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Spark not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;

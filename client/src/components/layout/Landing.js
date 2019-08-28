a4a66a625403b506ba894696515d060f002c8f8e
adudley78 <dudleyadam@gmail.com>
2019-08-28 13:00:10 -0400

    Clean up files and set up initial components

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Welcome to the Bros Philosophy Club!</h1>
+          <p className='lead'>Sign Up Now to Discuss Big Ideas</p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;

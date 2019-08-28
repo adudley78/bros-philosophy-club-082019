ddd0846694affd607a0f2d4e284ca56ce9780d9f
adudley78 <dudleyadam@gmail.com>
2019-08-28 10:29:15 -0400

    Build create and delete upvote routes

--- routes/api/sparks.js
+++ routes/api/sparks.js
@@ -132,4 +132,35 @@ router.put('/upvote/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/sparks/unupvote/:id
+// @desc    Un-upvote a spark
+// @access  Private
+router.put('/unupvote/:id', auth, async (req, res) => {
+  try {
+    const spark = await Spark.findById(req.params.id);
+
+    // Check if spark has already been upvoted by this user
+    if (
+      spark.upvotes.filter(upvote => upvote.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Spark has not yet been upvoted' });
+    }
+
+    // Get remove index
+    const removeIndex = spark.upvotes
+      .map(upvote => upvote.user.toString())
+      .indexOf(req.user.id);
+
+    spark.upvotes.splice(removeIndex, 1);
+
+    await spark.save();
+
+    res.json(spark.upvotes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;

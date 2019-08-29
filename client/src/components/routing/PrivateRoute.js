05b10338bbe2b94ac2573f5e7640a663c556b3ef
adudley78 <dudleyadam@gmail.com>
2019-08-29 10:36:04 -0400

    Build protected route component and use for dashboard

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// ...rest => Take anything that's past in with the Component
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);

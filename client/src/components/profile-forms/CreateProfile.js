964d1c5bcede41a86aeeff8b51ff08b0b7528464
adudley78 <dudleyadam@gmail.com>
2019-08-30 12:21:01 -0400

    Build main Profile component

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,245 @@
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = props => {
+  const [formData, setFormData] = useState({
+    profession: '',
+    website: '',
+    blog: '',
+    location: '',
+    status: '',
+    bio: '',
+    background: '',
+    beliefs: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  // Destructure
+  const {
+    profession,
+    website,
+    blog,
+    location,
+    status,
+    bio,
+    background,
+    beliefs,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = formData;
+
+  // Whatever the value is of the text field will be put into the state of the form data
+  const onChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+  return (
+    <Fragment>
+      {' '}
+      <h1 className='large text-primary'>Create Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Complete this form with as much quality
+        detail as possible to make your profile stand out!
+      </p>
+      <small>* = required field</small>
+      <form className='form'>
+        <div className='form-group'>
+          <select name='status' value={status} onChange={e => onChange(e)}>
+            <option value='0'>* Select Your Religion</option>
+            <option value='Christian'>Christian</option>
+            <option value='Christian-Catholic'>Christian-Catholic</option>
+            <option value='Christian-Protestantism'>
+              Christian-Protestantism
+            </option>
+            <option value='Christian-Other'>Christian-Other</option>
+            <option value='Islam'>Islam</option>
+            <option value='Secular/Nonreligious/Agnostic/Atheist'>
+              Unaffiliated: Secular/Nonreligious/Agnostic/Atheist
+            </option>
+            <option value='Hinduism'>Hinduism</option>
+            <option value='Buddhism'>Buddhism</option>
+            <option value='Folk Religion'>Folk Religion</option>
+            <option value='Judaism'>Judaism</option>
+            <option value='Other'>Other</option>
+          </select>
+          <small className='form-text'>
+            Give us an idea of how you most identify religiously
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Profession'
+            name='profession'
+            value={profession}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            Could be your vocation or avocation
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Website'
+            name='website'
+            value={website}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            Could be your own or a company website
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Blog'
+            name='blog'
+            value={blog}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            Could be your own or a company blog
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            City & state suggested (eg. Orlando, FL)
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={e => onChange(e)}
+          ></textarea>
+          <small className='form-text'>
+            Tell us a little about yourself (200 characters or less suggested)
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A brief summary of your background'
+            name='background'
+            value={background}
+            onChange={e => onChange(e)}
+          ></textarea>
+          <small className='form-text'>
+            Tell us a little about your experiences in the world that you think
+            make you who you are
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short summary of your beliefs'
+            name='beliefs'
+            value={beliefs}
+            onChange={e => onChange(e)}
+          ></textarea>
+          <small className='form-text'>
+            Tell us a little about your beliefs
+          </small>
+        </div>
+
+        <div className='my-2'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x'></i>
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </Fragment>
+        )}
+
+        <input type='submit' className='btn btn-primary my-1' />
+        <a className='btn btn-light my-1' href='dashboard.html'>
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
--- models/Profile.js
+++ models/Profile.js
@@ -5,82 +5,30 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
   },
-  // company: {
-  //   type: String
-  // },
+  profession: {
+    type: String
+  },
   website: {
+    type: String
+  },
+  blog: {
     type: String
   },
   location: {
     type: String
   },
   status: {
-    type: String,
-    required: true
+    type: [String]
   },
-  // skills: {
-  //   type: [String],
-  //   required: true
-  // },
   bio: {
     type: String
   },
-  // experience: [
-  //   {
-  //     title: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     company: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     from: {
-  //       type: Date,
-  //       required: true
-  //     },
-  //     to: {
-  //       type: Date
-  //     },
-  //     current: {
-  //       type: Boolean,
-  //       default: false
-  //     },
-  //     description: {
-  //       type: String
-  //     }
-  //   }
-  // ],
-  // education: [
-  //   {
-  //     school: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     degree: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     fieldofstudy: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     from: {
-  //       type: Date,
-  //       required: true
-  //     },
-  //     to: {
-  //       type: Date
-  //     },
-  //     current: {
-  //       type: Boolean,
-  //       default: false
-  //     },
-  //     description: {
-  //       type: String
-  //     }
-  //   }
-  // ],
+  background: {
+    type: String
+  },
+  beliefs: {
+    type: String
+  },
   social: {
     youtube: {
       type: String
